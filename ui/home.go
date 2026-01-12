302c3738c3cacf2ab844e32b4db03fe31e1596d8
Enzo Del-Rey <enzo.delrey@ynov.com>
2026-01-12 16:39:29 +0100

    new

--- ui/home.go
+++ ui/home.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"groupietracker/api"
+	"groupietracker/models"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -10,22 +11,36 @@ import (
 
 func BuildHome() fyne.CanvasObject {
 	artists, err := api.FetchArtists()
-
 	if err != nil {
 		return widget.NewLabel("Erreur de chargement")
 	}
 
 	list := container.NewVBox()
 
-	for _, artist := range artists {
-		a := artist
-
-		btn := widget.NewButton(a.Name, func() {
-			ShowArtistDetails(a)
-		})
-
-		list.Add(btn)
+	updateList := func(filtered []models.Artist) {
+		list.Objects = nil
+		for _, artist := range filtered {
+			a := artist
+			btn := widget.NewButton(a.Name, func() {
+				ShowArtistDetails(a)
+			})
+			list.Add(btn)
+		}
+		list.Refresh()
 	}
 
-	return container.NewVScroll(list)
+	updateList(artists)
+
+	search := widget.NewEntry()
+	search.SetPlaceHolder("Rechercher un groupe...")
+
+	search.OnChanged = func(text string) {
+		filtered := FilterArtists(artists, text)
+		updateList(filtered)
+	}
+
+	return container.NewBorder(
+		search, nil, nil, nil,
+		container.NewVScroll(list),
+	)
 }
--- ui/search.go
+++ ui/search.go
@@ -1,1 +1,20 @@
 package ui
+
+import (
+	"groupietracker/models"
+	"strings"
+)
+
+func FilterArtists(artists []models.Artist, query string) []models.Artist {
+	var result []models.Artist
+
+	query = strings.ToLower(query)
+
+	for _, artist := range artists {
+		if strings.Contains(strings.ToLower(artist.Name), query) {
+			result = append(result, artist)
+		}
+	}
+
+	return result
+}
