33fe401744357e4990645a0771d366e73010efe8
Florent Folliard <florent.folliard@ynov.com>
2026-01-16 15:06:21 +0100

    maj de la struct artiste (Locations/DatesConcerts/Relations)

--- models/artist.go
+++ models/artist.go
@@ -1,10 +1,17 @@
 package models
 
+// Artist représente la structure d'un artiste telle qu'elle apparaît
+// dans le tableau JSON de l'endpoint /api/artists
 type Artist struct {
 	ID           int      `json:"id"`
-	Name         string   `json:"name"`
 	Image        string   `json:"image"`
+	Name         string   `json:"name"`
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
+	// Cette variable stocke l'URL vers l'objet "locations" (ex: /api/locations/1)
+	Locations string `json:"locations"`
+	// Ces variables stockent les autres URLs de l'API
+	ConcertDates string `json:"concertDates"`
+	Relations    string `json:"relations"`
 }
